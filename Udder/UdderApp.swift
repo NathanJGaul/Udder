932e81c429b420b09968a00a4f8f9849c7532127
NathanJGaul <nathanjgaul@gmail.com>
2024-01-02 10:31:27 -0600

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Nathan Gaul on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
